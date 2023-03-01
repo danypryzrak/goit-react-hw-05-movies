7db740dfec61dbe15e5b5a01ffb73877c79a4059
danypryzrak <116654935+danypryzrak@users.noreply.github.com>
2023-03-01 10:30:03 +0100

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,7 +13,7 @@ export const App = () => {
 
   return (<>
   <Routes>
-    <Route path='/' element={<Layout/>} >
+    <Route path='/goit-react-hw-05-movies' element={<Layout/>} >
         <Route index element={<HomePage/>}/>
         <Route path='movies' element={<MoviesPage />}/>
         <Route path='movies/:id' element={<MovieDetails/>}>
